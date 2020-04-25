2a35fcbf20a79c5555b0d8b1ac896e4ba662ee39
SChan-Kim <instufld@gmail.com>
2020-04-26 00:17:43 +0900

    98

---
+++ 1098_12879955(AC).cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+int main() {
+
+    int h,w,n;
+    int l,d,x,y;
+    cin >> h >> w;
+
+    int map[100][100];
+
+    for(int i=1; i<=h; i++) {
+        for(int j=1; j<=w; j++)
+            map[i][j] = 0;
+    }
+    // initialization
+
+    cin >> n;
+
+    for(int i=0; i<n; i++) {
+        cin >> l >> d >> x >> y;
+        cout << "";
+        while(1) {
+            map[x][y] = 1;
+            l--;
+            if(l==0)
+                break;
+            else {
+                if(d==1)
+                    x++;
+                else
+                    y++;
+            }
+        }
+    }
+
+    for(int i=1; i<=h; i++) {
+        for(int j=1; j<=w; j++)
+            cout << map[i][j] << " ";
+        cout << endl;
+    }
+
+
+    return 0;
+}
+
--- test.cpp
+++ test.cpp
@@ -5,36 +5,39 @@ using namespace std;
 
 int main() {
 
-    int input, a, b;
-    int map[20][20];
+    int h,w,n;
+    int l,d,x,y;
+    cin >> h >> w;
 
-    for(int i=1; i<20; i++) {
-            for(int j=1; j<20; j++)
-                cin >> map[i][j];
+    int map[100][100];
+
+    for(int i=1; i<=h; i++) {
+        for(int j=1; j<=w; j++)
+            map[i][j] = 0;
     }
+    // initialization
 
-    cin >> input;
+    cin >> n;
 
-    for(int i=1; i<=input; i++) {
-        cin >> a >> b;
-        for(int j=1; j<20; j++) {
-            if(map[a][j] == 0)
-                map[a][j] = 1;
-            else
-                map[a][j] = 0;
-
+    for(int i=0; i<n; i++) {
+        cin >> l >> d >> x >> y;
+        cout << "";
+        while(1) {
+            map[x][y] = 1;
+            l--;
+            if(l==0)
+                break;
+            else {
+                if(d==1)
+                    x++;
+                else
+                    y++;
             }
-
-        for(int k=1; k<20; k++) {
-            if(map[k][b] == 0)
-                map[k][b] = 1;
-            else
-                map[k][b] = 0;
         }
     }
 
-    for(int i=1; i<20; i++) {
-        for(int j=1; j<20; j++)
+    for(int i=1; i<=h; i++) {
+        for(int j=1; j<=w; j++)
             cout << map[i][j] << " ";
         cout << endl;
     }
