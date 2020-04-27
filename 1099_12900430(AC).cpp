91a1478e39513b1991b9f249c9d5e9b0e2a58c1f
SChan-Kim <instufld@gmail.com>
2020-04-27 21:35:35 +0900

    99

---
+++ 1099_12900430(AC).cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+int main() {
+
+    int map[10][10];
+    int x,y;
+
+    for(int i=0; i<10; i++) {
+        for(int j=0; j<10; j++)
+            cin >> map[j][i];
+    }
+
+    x = 1;
+    y = 1;
+
+    while(1) {
+
+        if(map[1][1] == 2) {
+            map[1][1] = 9;
+            break;
+        }
+
+        map[x][y] = 9;
+
+        if((map[x+1][y] == 1)&&(map[x][y+1] == 1))
+            break;
+
+        else if(map[x+1][y] == 2) {
+            map[x+1][y] = 9;
+            break;
+        }
+
+        else if((map[x][y+1] == 2) && (map[x+1][y] == 1)) {
+            map[x][y+1] = 9;
+            break;
+        }
+
+        else if(map[x+1][y] == 1)
+            y++;
+
+        else if(map[x][y+1] == 1)
+            x++;
+        else
+            x++;
+    }
+
+    for(int i=0; i<10; i++) {
+        for(int j=0; j<10; j++)
+            cout << map[j][i] << " ";
+        cout << endl;
+    }
+
+    return 0;
+}
+
--- test.cpp
+++ test.cpp
@@ -5,43 +5,53 @@ using namespace std;
 
 int main() {
 
-    int h,w,n;
-    int l,d,x,y;
-    cin >> h >> w;
+    int map[10][10];
+    int x,y;
 
-    int map[100][100];
-
-    for(int i=1; i<=h; i++) {
-        for(int j=1; j<=w; j++)
-            map[i][j] = 0;
+    for(int i=0; i<10; i++) {
+        for(int j=0; j<10; j++)
+            cin >> map[j][i];
     }
-    // initialization
 
-    cin >> n;
+    x = 1;
+    y = 1;
 
-    for(int i=0; i<n; i++) {
-        cin >> l >> d >> x >> y;
-        cout << "";
-        while(1) {
-            map[x][y] = 1;
-            l--;
-            if(l==0)
-                break;
-            else {
-                if(d==1)
-                    x++;
-                else
-                    y++;
-            }
+    while(1) {
+
+        if(map[1][1] == 2) {
+            map[1][1] = 9;
+            break;
         }
+
+        map[x][y] = 9;
+
+        if((map[x+1][y] == 1)&&(map[x][y+1] == 1))
+            break;
+
+        else if(map[x+1][y] == 2) {
+            map[x+1][y] = 9;
+            break;
+        }
+
+        else if((map[x][y+1] == 2) && (map[x+1][y] == 1)) {
+            map[x][y+1] = 9;
+            break;
+        }
+
+        else if(map[x+1][y] == 1)
+            y++;
+
+        else if(map[x][y+1] == 1)
+            x++;
+        else
+            x++;
     }
 
-    for(int i=1; i<=h; i++) {
-        for(int j=1; j<=w; j++)
-            cout << map[i][j] << " ";
+    for(int i=0; i<10; i++) {
+        for(int j=0; j<10; j++)
+            cout << map[j][i] << " ";
         cout << endl;
     }
-
 
     return 0;
 }
